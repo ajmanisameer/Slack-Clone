b4c4e87eb073b7edbf804c59e02f983b15b8b626
Ajmani <ajmani@Schools-Mac-mini-2.local>
2020-02-15 15:09:41 +0530

    added socket connection

--- server.js
+++ server.js
@@ -1,35 +1,63 @@
-const express = require('express')
+const express = require("express");
 const app = express();
-const http = require('http')
-const dotenv = require('dotenv')
-const mongoose = require('mongoose')
-const bodyParser = require('body-parser')
+const path = require("path");
+const http = require("http");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const socketIO = require("socket.io");
 // const userRoutes = require('./routes/user')
 // const courseRoutes = require('./routes/course')
+
+//Server connection
+const port = process.env.PORT || 3000;
+const server = http.createServer(app);
+
+let io = socketIO(server);
 
 
 dotenv.config();
 
-
-mongoose.connect('mongodb+srv://aj:ajmani@cluster0-c60su.mongodb.net/test?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect(
+  "mongodb+srv://aj:ajmani@cluster0-c60su.mongodb.net/test?retryWrites=true&w=majority",
+  { useNewUrlParser: true, useUnifiedTopology: true }
+);
 const db = mongoose.connection;
-db.on('error', (error) => console.error(error));
-db.once('open', () => console.log('Database Connected'));
-
+db.on("error", error => console.error(error));
+db.once("open", () => console.log("Database Connected"));
 
 // Body parser
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.use(express.static(__dirname + '/public'));
-app.get('/', (req, res) => {
-    res.sendFile('/public', {root: __dirname})
-})
+
+//Listening to the io event
+io.on("connection", socket => {
+    console.log("A new User Connected");
+  
+    socket.on("disconnect", () => {
+      console.log("User Disconnected from Server");
+    });
+  });
+  
+  io.on("disconnect", () => {
+    console.log("User Disconnected from Server");
+  });
+
+
+  //Render Files
+app.use(express.static(__dirname + "/public"));
+app.get("/", (req, res) => {
+  res.sendFile("/public", { root: __dirname });
+});
 
 // app.use('/user', userRoutes)
 
 // app.use('/course', courseRoutes)
 
+// server.listen(port, () => {
+//     console.log(`Server is on $(port)`);
+// })
 
-http.createServer(app).listen(process.env.PORT || 3000);
+server.listen(process.env.PORT || 3000);
 console.log("BackEnd Server Is On=", process.env.PORT || 3000);
